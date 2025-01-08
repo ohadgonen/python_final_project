2c559e6c6bfd46a60202190960fe91630f4e447e
ohadgonen <ohadgonen0@gmail.com>
2025-01-08 11:51:46 +0200

    Sorted the files

--- python_final_project/script.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Change to the src directory
-os.chdir('/Users/ohadgonen/Desktop/Neuroscience/Year 2/1st semester/Advenced programming in Python/מטלות בית/python_final_project/src')
-print(f"Current working directory: {os.getcwd()}")
