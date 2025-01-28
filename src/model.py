8911df54f11c352f493372cb0ffd2bb8043757e1
ohadgonen <ohadgonen0@gmail.com>
2025-01-28 17:56:45 +0200

    project report

--- src/model.py
+++ src/model.py
@@ -2,7 +2,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
-from imblearn.over_sampling import SMOTE
 
 
 def preprocess_data(df):
